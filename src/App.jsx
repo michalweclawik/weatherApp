fea7acacb2c8ac815e97597c561ac15a5764b69b
michal <michalwagh@gmail.com>
2022-11-16 09:53:13 +0000

    deploy trial

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./containers/Home/Home";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as HashRouter, Routes, Route } from "react-router-dom";
 import TodosList from "./containers/TodosList/TodosList";
 function App() {
   const api_key = process.env.REACT_APP_API_KEY;
 
   return (
     <div className="App">
-      <Router>
+      <HashRouter>
         <Navbar />
         <Routes>
           <Route excat path="/" element={<Home api_key={api_key} />} />
           <Route excat path="/todos" element={<TodosList />} />
         </Routes>
-      </Router>
+      </HashRouter>
     </div>
   );
 }
